7d907d2099fe9e961d663872478f0d211734181a
= <thanhcong@gmail.com>
2014-07-21 15:10:56 +0700

    Finish controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 
+include ArticlesHelper
 
 def index
 	@articles=Article.all
@@ -7,5 +8,41 @@ end
 def show
 	@article = Article.find(params[:id])
 
+end
+
+def new
+	@article = Article.new
+end
+
+def create1
+	@article1 = Article.new
+	@article.title = params[:article][:title]
+	@article.body = params[:article][:body]
+	@article.save
+	redirect_to article_path(@article)
+end
+def create
+	@article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+
+end
+def destroy
+	@article = Article.find(params[:id]);
+	@article.destroy
+	redirect_to articles_path
+end
+
+def edit
+	@article = Article.find(params[:id])
+
+end
+def  update
+	@article = Article.find(params[:id])
+	@article.update(article_params)
+	  flash.notice = "Article '#{@article.title}' Updated!"
+
+	redirect_to article_path(@article)
 end
 end
